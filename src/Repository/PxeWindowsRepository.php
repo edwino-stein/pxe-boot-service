bf05481225d925644dc785a19776896c74af5472
Edwino Stein <edwino.stein@gmail.com>
2020-07-21 18:26:24 -0400

    Fix PxeWindowsRepository initrd parameters

--- src/Repository/PxeWindowsRepository.php
+++ src/Repository/PxeWindowsRepository.php
@@ -9,9 +9,9 @@ class PxeWindowsRepository extends AbstractPxeTargetRepository
     protected function setupModel(PxeTargetModel $model) : PxeTargetModel
     {
         $model->setKernel($this->getResourceService()->getTool('wimboot'))
-              ->pushInitrd('boot/BCD', ['name' => 'BCD'])
-              ->pushInitrd('boot/boot.sdi', ['name' => 'boot.sdi'])
-              ->pushInitrd('boot/boot.wim', ['name' => 'boot.wim']);
+              ->pushInitrd('boot/BCD', ['BCD'])
+              ->pushInitrd('boot/boot.sdi', ['boot.sdi'])
+              ->pushInitrd('boot/boot.wim', ['boot.wim']);
 
         return parent::setupModel($model);
     }
