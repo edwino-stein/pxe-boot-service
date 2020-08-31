66a5e5dfc18c97a5a2b37c6ec998436b247074c2
Edwino Stein <edwino.stein@gmail.com>
2020-08-30 21:13:13 -0400

    Add integration of WindowsNetInstallService with PxeWindowsRepository

--- src/Repository/PxeWindowsRepository.php
+++ src/Repository/PxeWindowsRepository.php
@@ -6,13 +6,37 @@ use App\Model\PxeTargetModel;
 
 class PxeWindowsRepository extends AbstractPxeTargetRepository
 {
+    protected $withNetinstall = false;
+    protected $netInstallConfigRoute = '/windows/{dist}/{arch}/netinstall.json';
+    protected $netInstallSupport = ['10', '8'];
+
     protected function setupModel(PxeTargetModel $model) : PxeTargetModel
     {
+        if($this->withNetinstall && in_array($model->getDist(), $this->netInstallSupport)){
+            $model->pushInitrd(
+                self::encodeStr(
+                    $this->netInstallConfigRoute,
+                    [
+                        '{dist}' => $model->getDist(),
+                        '{arch}' => $model->getArch()
+                    ]
+                ),
+                ['netinstall.json']
+            );
+        }
+
         $model->setKernel($this->getResourceService()->getTool('wimboot'))
               ->pushInitrd('boot/BCD', ['BCD'])
               ->pushInitrd('boot/boot.sdi', ['boot.sdi'])
               ->pushInitrd('boot/boot.wim', ['boot.wim']);
 
         return parent::setupModel($model);
+    }
+
+    protected function onSetup() : void
+    {
+        if(isset($this->getCfg()['netinstall']) && is_bool($this->getCfg()['netinstall'])){
+            $this->withNetinstall = $this->getCfg()['netinstall'];
+        }
     }
 }
