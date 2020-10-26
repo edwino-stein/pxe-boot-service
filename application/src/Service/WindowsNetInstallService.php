8c4569ed6c26187ee591adf5616539d2b967257d
Edwino Stein <edwino.stein@gmail.com>
2020-10-26 16:51:39 -0400

    Use environment variable of smb server to setup windows netinstall
    Issue #1

--- application/src/Service/WindowsNetInstallService.php
+++ application/src/Service/WindowsNetInstallService.php
@@ -7,29 +7,27 @@ use App\Model\PxeTargetModel;
 
 class WindowsNetInstallService
 {
-    protected $pes = null;
+    protected $prs = null;
     protected $smbServer = '127.0.0.1';
     protected $smbBasePath = 'pxe';
     protected $repository = 'windows';
     protected $setup = 'setup.exe';
 
-    public function __construct(ContainerInterface $container, PxeResourceService $pes)
+    public function __construct(ContainerInterface $container, PxeResourceService $prs)
     {
         $config = $container->getParameter('windows_netinstall');
 
         if(isset($config['smbServer'])) $this->smbServer = $config['smbServer'];
-        elseif(isset($_ENV['NEXT_SERVER'])) $this->smbServer = $_ENV['NEXT_SERVER'];
-
         if(isset($config['smbBasePath'])) $this->smbBasePath = $config['smbBasePath'];
         if(isset($config['repository'])) $this->repository = $config['repository'];
         if(isset($config['setup'])) $this->setup = $config['setup'];
 
-        $this->pes = $pes;
+        $this->prs = $prs;
     }
 
     private function getBuilds(string $dist, string $arch): array
     {
-        $finder = $this->pes->getRepositoryFinder($this->repository);
+        $finder = $this->prs->getRepositoryFinder($this->repository);
         $finder ->path(PxeResourceService::concatPath([$dist, $arch, '']))
                 ->files()
                 ->name($this->setup)
